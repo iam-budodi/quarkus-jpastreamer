6d5f3b9386259373d9b42a540b40a3254c7afda3
quarkusio <no-reply@quarkus.io>
2023-11-06 16:44:37 +0000

    Initial commit

---
+++ src/test/java/org/japhet/sebastian/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.japhet.sebastian;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
